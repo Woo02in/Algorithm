5a0c75a6ba410c78616e722339abcf13cb6a2ab6
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-28 00:21:01 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int>v;
+int main() {
+	int N, K;
+	cin >> N >> K;
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			int a;
+			v.push_back(a);
+		}
+	}
+
+	cout << "비와이" << endl;
+
+	return 0;
+}
