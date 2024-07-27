95c27a2e4f9873e36f1e72547ab2b35d666477c7
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-27 17:20:09 +0900

    [Platinum V] Title: 히스토그램, Time: 44 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<queue>
+#include<utility>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+long long n;
+long long histogram[100001];
+int main() {
+    cin>>n;
+	for (int i = 0; i < n; i++) {
+		cin >> histogram[i];
+	}		
+	histogram[n] = -1;
+	stack<int> s;
+	long long answer = 0;
+	for (int i = 0; i <= n; i++) {
+		while (!s.empty() && histogram[i] < histogram[s.top()]) {
+			int height, width;
+			height = s.top();
+			s.pop();
+			if (s.empty())
+				width = i;
+			else
+				width = i - s.top()-1;
+			answer = max(answer, histogram[height] * width);				
+		}
+		s.push(i);
+	}
+	cout << answer << endl;		
+	
+	return 0;
+}
