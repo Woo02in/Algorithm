b9f7a3b2f18f398c3494d7f3a4f727459d4883a5
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-27 17:51:51 +0900

    [Bronze IV] Title: 타임 카드, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/5575. 타임 카드/타임 카드.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    for (int i = 0; i < 3; i++) {
+        int sh, sm, ss, eh, em, es;
+        cin >> sh >> sm >> ss >> eh >> em >> es;
+        if (es - ss < 0) {    em -= 1, es = 60 + es - ss; }
+        else { es = es - ss; }
+        if (em - sm < 0) { eh -= 1, em = 60 + em - sm; }
+        else { em = em - sm; }
+        eh = eh - sh;
+        cout << eh << " " << em << " " << es << '\n';
+    }
+    return 0;
+}
