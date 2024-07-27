58897c0445fb4a30be1ed0505599070226e1ee68
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-28 00:23:08 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt, int min) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			if (i < min)
+				continue;
+			visited[i] = true;
+			arr[cnt] = i;
+			dfs(cnt + 1,i);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	dfs(0,0);
+
+	return 0;
+}
