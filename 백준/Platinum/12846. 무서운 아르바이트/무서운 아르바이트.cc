2960a5c223c7313e4269baf1d690e1495c83ae90
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-28 00:18:21 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<queue>
+#include<utility>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+long long n;
+long long histogram[100001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;	
+	for (int i = 0; i < n; i++) {
+		cin >> histogram[i];
+	}
+	histogram[n] = -1;
+	stack<int> s;
+	long long answer = 0;
+	for (int i = 0; i <= n; i++) {
+		while (!s.empty() && histogram[i] <= histogram[s.top()]) {
+			int height, width;
+			height = s.top();
+			s.pop();
+			if (s.empty())
+				width = i;
+			else
+				width = i - s.top() - 1;
+			answer = max(answer, histogram[height] * width);
+		}
+		s.push(i);
+	}
+	cout << answer << endl;
+	return 0;
+}
