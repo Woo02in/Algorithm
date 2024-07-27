63f20c6c38e475ae8430822514467abee85b36fb
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-28 00:23:25 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	if (!N)
+		cout << "YONSEI" << endl;
+	else
+		cout << "Leading the Way to the Future" << endl;
+	return 0;
+}
