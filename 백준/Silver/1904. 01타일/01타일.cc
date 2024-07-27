4e637e30874540a55bb05114217904b1a4912a63
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-27 17:23:09 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5016 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int DP[1000000] = { 0, };
+int main() {
+	int N;
+	scanf("%d", &N);
+	DP[1] = 1;
+	DP[2] = 2;
+	for (int i = 3; i <= N; ++i) {
+		DP[i] = (DP[i - 2] + DP[i - 1]) % 15746;
+	}
+	
+	printf("%d", DP[N]);
+
+	return 0;
+
+}
