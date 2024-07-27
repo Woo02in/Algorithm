d631b4c8cc55e44e0591c54e9e0b47ab31ac5579
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-28 00:14:28 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int dp[1001][10];
+int ans, N;
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = j; k < 10; k++) {
+				dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		ans = (ans + dp[N][i]) % 10007;
+	}
+	cout << ans;
+
+	return 0;
+}
