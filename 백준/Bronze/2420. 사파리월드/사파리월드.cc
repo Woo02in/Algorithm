a35e8e69bbfc668fa5de528b97961b60027c0920
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-27 17:32:07 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	long long ans = 0;
+	ans = abs(a - b);
+	cout << ans;
+
+
+	return 0;
+}
