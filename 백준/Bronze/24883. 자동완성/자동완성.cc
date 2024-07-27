e4756bed4a4b80ca1c180bfdf329d0d369af8591
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-07-28 00:32:04 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	char check;
+	cin >> check;
+	if (check == 'N' || check == 'n') {
+		cout << "Naver D2" << endl;
+	}
+	else {
+		cout << "Naver Whale" << endl;
+	}
+
+}
