1d72817b1f51620e94892896f84518b952a91942
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-08-25 22:13:09 +0900

    [Silver III] Title: 개미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int w, h, p, q, t;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> w >> h >> p >> q >> t;
+
+    if ((p + t) / w % 2 == 0) {
+        cout << (p + t) % w << ' ';
+    }
+    else cout << w - (p + t) % w << ' ';
+
+    if ((q + t) / h % 2 == 0) {
+        cout << (q + t) % h;
+    }
+    else cout << h - (q + t) % h;
+}
