936e06f564a010ade1fce21464b71176d6acafb1
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-08-26 14:01:36 +0900

    [Silver V] Title: 피보나치 수 4, Time: 200 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+string DP[10010];
+
+string sum(string x, string y)
+{
+	int num;
+	int carry = 0;
+	string result;
+
+	reverse(x.begin(), x.end());
+	reverse(y.begin(), y.end());
+
+	while (x.length() < y.length()) {
+		x += '0';
+	}
+	while (x.length() > y.length()) {
+		y += '0';
+	}
+
+	for (int i = 0; i < x.length(); ++i) {
+		num = (x[i] - '0' + y[i] - '0' + carry) % 10;
+		result += to_string(num);
+		carry = (x[i] - '0' + y[i] - '0' + carry) / 10;
+	}
+	if (carry != 0) {
+		result += to_string(carry);
+	}
+
+	reverse(result.begin(), result.end());
+
+	return result;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	cin >> n;
+	
+	DP[0] = '0';
+	DP[1] = '1';
+
+	for (int i = 2; i <= n; ++i) {
+		DP[i] = sum(DP[i - 1], DP[i - 2]);
+	}
+	cout << DP[n] << endl;
+	return 0;
+}
