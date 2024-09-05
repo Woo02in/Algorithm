46ff91c278a1cb88d94403c3676bd427648675c8
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-09-05 18:31:20 +0900

    [Bronze I] Title: 달팽이2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/1952. 달팽이2/달팽이2.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int map[101][101];
+int dr[4] = { 0,1,0,-1 };
+int dc[4] = { 1,0,-1,0 };
+
+int main() {
+
+    int M, N;
+    cin >> M >> N;    
+    int ans = 0;
+    int cnt = 1;
+
+    int d = 0;
+    int r = 0;
+    int c = 0;
+    map[r][c] = 1;
+    while (1) {
+
+        if (cnt == M * N) break;
+
+        int nr = r + dr[d];
+        int nc = c + dc[d];
+      
+        if (nr<0 || nr>M - 1 || nc<0 || nc>N - 1 || map[nr][nc] == 1) {
+            ans += 1;
+            if (d == 3) d = 0;
+            else d = d + 1;
+        }
+        else {
+            map[nr][nc] = 1;
+            cnt += 1;
+            r = nr;
+            c = nc;
+        }
+    }
+    cout << ans;
+    return 0;
+}
