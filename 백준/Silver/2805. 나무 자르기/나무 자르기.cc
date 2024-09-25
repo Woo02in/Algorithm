84b4a886d38e9ca4e1aaefc2cf2292d38f6ec5a2
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-09-25 20:40:38 +0900

    [Silver II] Title: 나무 자르기, Time: 156 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -3,14 +3,13 @@
 #include<vector>
 
 using namespace std;
-
+vector<int> v;
+long long ans = 0;
+long long N, M;
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
-
-	vector<int> v;
-	long long ans = 0;
-	long long N, M;
+	
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		int x;
@@ -20,7 +19,7 @@ int main() {
 	sort(v.begin(), v.end());
 	long long left = 1;
 	long long right = v[v.size() - 1];
-	
+
 	while (left <= right) {
 		long long sum = 0;
 		long long mid = (left + right) / 2;
@@ -36,7 +35,7 @@ int main() {
 			left = mid + 1;
 		}
 		else right = mid - 1;
-		
+
 	}
 	cout << ans << "\n";
 	return 0;
