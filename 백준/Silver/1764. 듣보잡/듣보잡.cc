fffd9397323341958d3ce64f4591481efbd23bff
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-09-25 19:48:28 +0900

    [Silver IV] Title: 듣보잡, Time: 68 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <iostream>
 #include <map>
-#include <string>
+#include <cstring>
 using namespace std;
+int N, M, cnt = 0;
+string s;
+map<string, int> m;
+map<string, int>::iterator iter;
 
 int main() {
-    int N, M, cnt = 0;
-    string s;
-    map<string, int> m;
-    map<string, int>::iterator iter;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);    
 
     cin >> N >> M;
-
     for (int i = 0; i < N + M; i++) {
         cin >> s;
         m[s]++;
         if (m[s] == 2) cnt++;
     }
-
-    cout << cnt << endl;
-
+    cout << cnt << '\n';
     for (iter = m.begin(); iter != m.end(); iter++)
         if (iter->second == 2)
-            cout << iter->first << endl;
-
-
+            cout << iter->first << '\n';
     return 0;
 }
