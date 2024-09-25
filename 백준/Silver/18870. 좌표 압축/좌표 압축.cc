9e691ff1ce712e8189a0ebc2fef41d16982c0040
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-09-25 20:43:09 +0900

    [Silver II] Title: 좌표 압축, Time: 416 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -3,25 +3,22 @@
 #include <algorithm>
 
 using namespace std;
-
-int main(){
+int N;
+int main() {
     ios::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int N;
+    cin.tie(0);
+    cout.tie(0);
+    
     cin >> N;
     vector<int> v(N);
-
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {
         cin >> v[i];
     }
-
     vector<int> ans(v);
     sort(ans.begin(), ans.end());
     ans.erase(unique(ans.begin(), ans.end()), ans.end());
 
-    for (const auto& val : v)
-    {
+    for (const auto& val : v){
         auto it = lower_bound(ans.begin(), ans.end(), val);
         cout << distance(ans.begin(), it) << ' ';
     }
