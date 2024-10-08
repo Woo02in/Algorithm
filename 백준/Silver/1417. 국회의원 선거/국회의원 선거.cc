accb5d7643d81c7e5de899071877c6f9d458cbf1
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2024-10-08 14:07:26 +0900

    [Silver V] Title: 국회의원 선거, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int vote[1000];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int count = 0;
+	int candidate;
+	int dasom;
+
+	
+
+	cin >> candidate;
+	cin >> dasom;
+
+	for (int i = 0; i < candidate - 1; i++) {
+		cin >> vote[i];
+	}
+
+	while (true) {
+		int find_max = 0;
+		int index = 0;
+		for (int i = 0; i < candidate - 1; i++) {
+			if (find_max < vote[i]) {
+				find_max = vote[i];
+				index = i;
+			}
+		}
+		if (dasom > find_max) {
+			cout << count << "\n";
+			break;
+		}
+		
+		dasom++;
+		vote[index]--;
+		count++;
+	}
+
+}
