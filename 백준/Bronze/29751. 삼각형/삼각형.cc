281800418d482a2dd8bacb22b01d138ec33049e1
Wooin Choi <133094473+Woo02in@users.noreply.github.com>
2025-06-22 17:46:20 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+#include<stdio.h>
+
+using namespace std;
+
+double w,h;
+double answer;
+
+int main(){
+    cin>>w>>h;
+    answer = w*h/2;
+    printf("%.1f\n", answer);
+
+
+    return 0;
+}
